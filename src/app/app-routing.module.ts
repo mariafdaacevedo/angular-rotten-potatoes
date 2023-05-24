8772d2223ae29b7d9744c7b0bad5861acc9d08f2
mariafdacevedo <113567795+mariafdaacevedo@users.noreply.github.com>
2023-05-24 15:47:53 -0500

    Updatte view of homepage fpr user

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { MovieComponent } from './movie/movie.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', redirectTo: 'login', pathMatch: 'full'}, //establecer una página de inicio de sesión como la primera página visible para los usuarios.
+  { path:'login', component: LoginComponent},
+  { path:'home', component: HomeComponent},
+  { path: 'movie', component: MovieComponent},
+  { path: '**', component: LoginComponent}  // capturar cualquier ruta no definida explícitamente y se utiliza como una ruta comodín o genérica.
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
